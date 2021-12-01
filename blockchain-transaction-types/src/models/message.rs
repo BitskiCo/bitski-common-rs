2cf958bf3eba8bc0a3b8f09d0073ed573d5cc139
Patrick Tescher <patrick@tescher.me>
2021-12-01 10:05:04 -0800

    Add generic message type

--- blockchain-transaction-types/src/models/message.rs
+++ blockchain-transaction-types/src/models/message.rs
@@ -1,4 +1,5 @@
 use crate::models::error::Error;
+use std::future::Future;
 
 pub trait Message {
     fn from_json(json: serde_json::Value) -> Result<Self, Error>
@@ -15,4 +16,36 @@ pub trait Message {
 pub enum MessageInfo {
     String(String),
     Json(serde_json::Value),
+}
+
+pub trait SignableMessage: Message {
+    fn message_hash(&self, chain_id: u64) -> Result<Vec<u8>, Error>;
+}
+
+use thiserror::Error as ThisError;
+
+#[derive(Debug, ThisError)]
+pub enum SignError<E> {
+    Hash(Error),
+    Sign(E),
+}
+
+impl dyn SignableMessage {
+    pub async fn sign_message<
+        E,
+        O: Future<Output = Result<(Vec<u8>, u64), E>>,
+        F: FnOnce(Vec<u8>) -> O,
+    >(
+        &self,
+        chain_id: u64,
+        provider: F,
+    ) -> Result<(Vec<u8>, u64), SignError<E>> {
+        let hash = self
+            .message_hash(chain_id)
+            .map_err(|error| SignError::<E>::Hash(error))?;
+        let (signature, recovery) = provider(hash)
+            .await
+            .map_err(|error| SignError::<E>::Sign(error))?;
+        Ok((signature, recovery))
+    }
 }
