32118a8878b97ba8d6847daf5ac2949810367038
Jacob Lee <jacob@bitski.com>
2022-05-23 11:22:14 -0700

    PgPool::from_env() always returns a production pool (#27)

--- bitski-common/src/diesel.rs
+++ bitski-common/src/diesel.rs
@@ -2,16 +2,15 @@
 
 use async_trait::async_trait;
 use diesel::r2d2::ConnectionManager;
-use diesel::Connection as _;
 use r2d2::{Pool, PooledConnection};
 
 use crate::env::parse_env_or;
 use crate::task::spawn_blocking;
 use crate::{Error, Result};
 
-const DEFAULT_DATABASE_URL: &str = "postgres://root@localhost:5432/defaultdb";
-const DEFAULT_DATABASE_POOL_MIN_IDLE: u32 = 1;
-const DEFAULT_DATABASE_POOL_MAX_SIZE: u32 = 4;
+pub const DEFAULT_DATABASE_URL: &str = "postgres://root@localhost:5432/defaultdb";
+pub const DEFAULT_DATABASE_POOL_MIN_IDLE: u32 = 1;
+pub const DEFAULT_DATABASE_POOL_MAX_SIZE: u32 = 4;
 
 /// PostgreSQL connection.
 pub type PgConnection = diesel::pg::PgConnection;
@@ -90,20 +89,18 @@ impl PgPoolExt for PgPool {
 
         let manager = ConnectionManager::<PgConnection>::new(database_url);
 
-        let builder = Pool::builder().min_idle(Some(min_idle)).max_size(max_size);
-        #[cfg(feature = "test")]
-        let builder = builder.min_idle(Some(1)).max_size(1);
-
-        let pool = builder.build(manager)?;
-
-        #[cfg(feature = "test")]
-        pool.get()?.begin_test_transaction()?;
+        let pool = Pool::builder()
+            .min_idle(Some(min_idle))
+            .max_size(max_size)
+            .build(manager)?;
 
         Ok(pool)
     }
 
     #[cfg(feature = "test")]
     fn for_test() -> Result<Self> {
+        use diesel::Connection as _;
+
         let database_url: String = parse_env_or("DATABASE_URL", DEFAULT_DATABASE_URL)?;
 
         let manager = ConnectionManager::<PgConnection>::new(database_url);
